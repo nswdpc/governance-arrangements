b67ad5088a3159b4a0558276e4265cd54751116f
James (DPC) <69664712+JamesDPC@users.noreply.github.com>
2025-05-30 13:15:59 +1000

    Add 20250516

---
+++ tests/v20250516Test.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace NSWDPC\GovernanceArrangements\Test;
+
+require_once( dirname(__FILE__) . '/GovernanceArrangementsServiceTest.php' );
+
+/**
+ * Test for 20250516
+ */
+class v20250516Test extends GovernanceArrangementsServiceTest {
+
+    /**
+     * @var float
+     */
+    protected $dataVersion = 20250516;
+
+    /**
+     * @var int|null
+     */
+    protected $totalClusters = 11;
+
+    public function testClusterDepartments() {
+        $data = $this->serviceInstance->getData();
+        $clusterData = $this->serviceInstance->getCluster('Premier and Cabinet');
+        $this->assertEquals(2, count($clusterData['departments']));
+    }
+
+    public function testClusterFromDepartment() {
+        $data = $this->serviceInstance->getData();
+        $clusterName = $this->serviceInstance->getClusterFromDepartment('Premier\'s Department');
+        $this->assertEquals('Premier and Cabinet', $clusterName);
+    }
+
+    public function testGetClusterFromAgency() {
+        $data = $this->serviceInstance->getData();
+        $cluster = $this->serviceInstance->getClusterFromAgency('Premier\'s Department');
+        $this->assertEquals('Premier and Cabinet', $cluster );
+    }
+}
